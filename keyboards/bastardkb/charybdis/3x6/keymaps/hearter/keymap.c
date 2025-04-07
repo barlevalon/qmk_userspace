8805b01cd3612a756a1027554f8ef8cb8154404a
Alon Hearter <barlevalon@users.noreply.github.com>
2025-04-08 00:20:52 +0300

    add enter,bsps to nav layer

--- keyboards/bastardkb/charybdis/3x6/keymaps/hearter/keymap.c
+++ keyboards/bastardkb/charybdis/3x6/keymaps/hearter/keymap.c
@@ -134,7 +134,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    KC_END,  KC_PGDN, KC_PGUP, KC_HOME, XXXXXXX, XXXXXXX,
   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
-                                  XXXXXXX, _______, XXXXXXX,    XXXXXXX, _______
+                                  XXXXXXX, _______, XXXXXXX,    KC_ENT,  KC_BSPS
   //                            ╰───────────────────────────╯ ╰──────────────────╯
   ),
 
